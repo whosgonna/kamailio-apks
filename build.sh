80e4d1f19356f6133dd65bcd78b9da2e7973563f
Kaufman <bkaufman@bcmone.com>
2024-08-05 17:17:28 -0500

    APKBUILD 5.6.6

--- build.sh
+++ build.sh
@@ -11,14 +11,15 @@ docker build \
     -t apk_builder:alpine-${ALPINE_VERSION} \
     .
 
+CONATINER_NAME="kam-apk-alp${ALPINE_VERSION}-kam${KAM_TARGET_VERSION}"
+
 docker run \
-    --rm \
     --env-file .env \
     -v ./abuild:/home/builder/.abuild \
     apk_builder:alpine-${ALPINE_VERSION} \
     build-apks.sh ${KAM_TARGET_VERSION}
 
-
+# --name ${CONTAINER_NAME} \
 
 
 
